e1401bc86b887426226637b45d990a0d5babab16
Nivit24 <91422309+Nivit24@users.noreply.github.com>
2021-09-27 12:03:52 +0530

    Add files via upload

---
+++ components/Project.js
@@ -0,0 +1,36 @@
+import react,{Component} from 'react';
+
+class Project extends Component{
+
+    render(){
+        return(
+            <div >
+                <h1>
+               Have A nice Day
+                </h1>
+            </div>
+
+        )
+    }
+}
+
+export default Project;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ components/Timber.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Timber() {
+
+    return(
+
+        <div>
+            <h1>
+                Assignment is done
+            </h1>
+        </div>
+    )
+}
+   
+export default Timber;
